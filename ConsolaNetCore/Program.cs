af990a9b9372aaeea63c3c50e9cfb21c84e00272
Patricio Cordoba <miloscordoba@gmail.com>
2020-09-24 02:29:57 -0300

    creando e inicializando lista de alumnos

--- ConsolaNetCore/Program.cs
+++ ConsolaNetCore/Program.cs
@@ -9,6 +9,8 @@ namespace ConsolaNetCore
     {
         static void Main(string[] args)
         {
+            //Programa de gestion de notas y asistencia de alumnos
+
             //Hacer un programa que ingrese una lista de alumnos, coloque sus notas de:
             //1er parcial, 2do parcial y final.
             //Se promociona con promedio 13, se aprueba con 4, la maxima discrepancia es un parcial 4 y otro parcial 9 o 10.
@@ -16,7 +18,41 @@ namespace ConsolaNetCore
             //TODO agregar mas consignas
 
             //primero harcodeado
-            List<Alumno> alumnos = new List<Alumno>();
+
+            //inicializando lista de alumnos
+            List<string> alumnos = new List<string>();
+
+            for (int i = 0; i < 20; i++)
+            {
+                alumnos.Add("alumno" + i);
+            }
+
+            Console.WriteLine("\nPrograma de gestion de notas y asistencia de alumnos\n\nA continuacion se le pedira agregar los alumnos y sus notas\n\nPresione cualquier tecla para continuar");
+
+            Console.ReadKey();
+
+
+            Console.WriteLine("TODO ingresar alumnos");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nSu alumno es: " + alumnos[i]);
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("\nPresione cualquier tecla para continuar");
+
+            Console.ReadKey();
+            
+
+
+            //TODO pedir ingreso de alumnos
+            //foreach (var alumno in alumnos)
+            //{
+            //    Console.WriteLine("");
+            //}
         }
     }
 }
