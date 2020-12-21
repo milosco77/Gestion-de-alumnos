7654416e3f905d947c3374684f0be25b533df2f2
Patricio Cordoba <miloscordoba@gmail.com>
2020-12-21 19:46:50 -0300

    se arregla errores de mensajes dentro de metodo EditarAsignatura, falta resolver excepcion

--- ConsolaNetCore/MetodosEditar.cs
+++ ConsolaNetCore/MetodosEditar.cs
@@ -30,14 +30,14 @@ namespace ConsolaNetCore
                             MetodosComunes.MensajeColor(mensaje: "\nEl alumno no existe.", color: ConsoleColor.Red);
                         }
                     } while (alumno == null);
-                    MetodosComunes.MensajeColor(mensaje: $"\nNombre: {alumno.Nombre}");
-                    alumno.Nombre = MetodosComunes.ValidacionTexto(mensajeIngreso: "\nIndique el nuevo nombre:");
-                    MetodosComunes.MensajeColor(mensaje: $"\nApellido: {alumno.Apellido}");
-                    alumno.Apellido = MetodosComunes.ValidacionTexto(mensajeIngreso: "\nIndique el nuevo apellido:");
-                    MetodosComunes.MensajeColor(mensaje: $"\nEdad: {alumno.Edad}");
-                    alumno.Edad = (byte)MetodosComunes.ValidacionNumericaInt(mensajeIngreso: "\nIndique la nueva edad:", minimoValorInput: 1, mensajeError: "\nEl valor debe ser mayor a 1.");
-                    MetodosComunes.MensajeColor(mensaje: $"\nDNI: {alumno.Dni}");
-                    alumno.Dni = MetodosComunes.ValidacionNumericaInt(mensajeIngreso: "\nIndique el nuevo DNI:", minimoValorInput: 11111111, mensajeError: "\nEl valor debe ser mayor a 11.111.111");
+                    MetodosComunes.MensajeColor(mensaje: $"\nAnterior Nombre: {alumno.Nombre}");
+                    alumno.Nombre = MetodosComunes.ValidacionTexto(mensajeIngreso: "\nNuevo Nombre:");
+                    MetodosComunes.MensajeColor(mensaje: $"\nAnterior Apellido: {alumno.Apellido}");
+                    alumno.Apellido = MetodosComunes.ValidacionTexto(mensajeIngreso: "\nNuevo Apellido:");
+                    MetodosComunes.MensajeColor(mensaje: $"\nAnterior Edad: {alumno.Edad}");
+                    alumno.Edad = (byte)MetodosComunes.ValidacionNumericaInt(mensajeIngreso: "\nNueva Edad:", minimoValorInput: 1, mensajeError: "\nEl valor debe ser mayor a 1.");
+                    MetodosComunes.MensajeColor(mensaje: $"\nAnterior DNI: {alumno.Dni}");
+                    alumno.Dni = MetodosComunes.ValidacionNumericaInt(mensajeIngreso: "\nNuevo DNI:", minimoValorInput: 11111111, mensajeError: "\nEl valor debe ser mayor a 11.111.111");
                     devolucionEditar = Logica.Alumno.Editar(alumno);
                     if (devolucionEditar.Contains("correctamente"))
                     {
@@ -55,6 +55,9 @@ namespace ConsolaNetCore
         {
             int cantidad = MetodosComunes.ValidacionNumericaInt(mensajeIngreso: $"\nCuantas asignaturas quiere editar (1-50):", mensajeError: "Valor no comprendido entre 1 y 50", minimoValorInput: 1, maximoValorInput: 50);
             Entidades.Asignaturas asignatura;
+            Entidades.ListadoAsignaturas listadoAsignatura;
+            Entidades.Alumnos alumno;
+            Entidades.Carreras carrera;
             int ID, horas, minutos;
             string devolucionEditar;
             for (int i = 0; i < cantidad; i++)
@@ -75,40 +78,92 @@ namespace ConsolaNetCore
                             MetodosComunes.MensajeColor(mensaje: "\nLa Asignatura no existe.", color: ConsoleColor.Red);
                         }
                     } while (asignatura == null);
-                    MetodosComunes.MensajeColor(mensaje: $"\nListadoAsignaturasID: {asignatura.ListadoAsignaturasId}");
-                    asignatura.ListadoAsignaturasId = MetodosComunes.ValidacionNumericaInt(mensajeIngreso: "\nIndique el nuevo ListadoAsignaturasID:", minimoValorInput: 1, mensajeError: "\nEl valor debe ser mayor que 1");
-                    MetodosComunes.MensajeColor(mensaje: $"\nAlumnoID: {asignatura.AlumnoId}");
-                    asignatura.AlumnoId = MetodosComunes.ValidacionNumericaInt(mensajeIngreso: "\nIndique el nuevo AlumnoID:", minimoValorInput: 1, mensajeError: "\nEl valor debe ser mayor que 1");
-                    MetodosComunes.MensajeColor(mensaje: $"\nCarreraID: {asignatura.CarreraId}");
-                    asignatura.CarreraId = MetodosComunes.ValidacionNumericaInt(mensajeIngreso: "\nIndique la nueva CarreraID:", minimoValorInput: 1, mensajeError: "\nEl valor debe ser mayor a 1.");
-                    MetodosComunes.MensajeColor(mensaje: $"\nComision: {asignatura.Comision}");
-                    asignatura.Comision = MetodosComunes.ValidacionNumericaInt(mensajeIngreso: "\nIndique la nueva Comision:", minimoValorInput: 1, mensajeError: "\nEl valor debe ser mayor a 1.");
-
-                    Console.WriteLine("\nIngrese el nuevo horario de entrada de la materia");
-                    MetodosComunes.MensajeColor(mensaje: $"\nHora: {asignatura.HorarioEntrada.Hours}");
-                    horas = MetodosComunes.ValidacionNumericaInt(mensajeIngreso: "\nIndique la nueva hora entre 0 y 23:", mensajeError: "\nIngrese un valor entre 0 y 23", minimoValorInput: 0, maximoValorInput: 23);
-                    MetodosComunes.MensajeColor(mensaje: $"\nMinutos: {asignatura.HorarioEntrada.Minutes}");
-                    minutos = MetodosComunes.ValidacionNumericaInt(mensajeIngreso: "\nIndique los nuevos minutos entre 0 y 59:", mensajeError: "\nIngrese un valor entre 0 y 59", minimoValorInput: 0, maximoValorInput: 59);
-                    asignatura.HorarioEntrada = new TimeSpan(hours: horas, minutes: minutos, seconds: 0);
-
-                    Console.WriteLine("\nIngrese el nuevo horario de salida de la materia");
-                    MetodosComunes.MensajeColor(mensaje: $"\nHora: {asignatura.HorarioSalida.Hours}");
-                    horas = MetodosComunes.ValidacionNumericaInt(mensajeIngreso: "\nIngrese la hora entre 0 y 23:", mensajeError: "\nIngrese un valor entre 0 y 23", minimoValorInput: 0, maximoValorInput: 23);
-                    MetodosComunes.MensajeColor(mensaje: $"\nMinutos: {asignatura.HorarioSalida.Minutes}");
-                    minutos = MetodosComunes.ValidacionNumericaInt(mensajeIngreso: "\nIngrese los minutos entre 0 y 59:", mensajeError: "\nIngrese un valor entre 0 y 59", minimoValorInput: 0, maximoValorInput: 59);
-
-                    asignatura.HorarioSalida = new TimeSpan(hours: horas, minutes: minutos, seconds: 0);
-                    MetodosComunes.MensajeColor(mensaje: $"\nDias: {asignatura.Dias}");
-                    asignatura.Dias = MetodosComunes.ValidacionTexto(mensajeIngreso: "\nIndique los nuevos dias:", mensajeError: "\nEl valor debe estar comprendido en Lunes-Martes-Miercoles-Jueves-Viernes-Sabado-Domingo");
-
-                    devolucionEditar = Logica.Asignatura.Editar(asignatura);
-                    if (devolucionEditar.Contains("correctamente"))
+                    if (MetodosInformar.InformarListadoAsignaturas() == 0)
                     {
-                        MetodosComunes.MensajeColor(mensaje: $"\nLa asignatura con ID: {asignatura.AsignaturaId} AlumnoID: {asignatura.AlumnoId} Comision: {asignatura.Comision} ha sido editaado correctamente.");
+                        MetodosComunes.MensajeColor(mensaje: "\nEl listado de Asignaturas esta vacio. Se debe tener al menos una asignatura en la tabla ListadoAsignaturas.", color: ConsoleColor.Red);
+                        MetodosComunes.Continuar();
                     }
                     else
                     {
-                        MetodosComunes.MensajeColor(mensaje: $"\n{devolucionEditar}", color: ConsoleColor.Red);
+                        do
+                        {
+                            MetodosComunes.MensajeColor(mensaje: $"\nAnterior ListadoAsignaturasID: {asignatura.ListadoAsignaturasId}");
+                            ID = asignatura.ListadoAsignaturasId = MetodosComunes.ValidacionNumericaInt(mensajeIngreso: "\nNuevo ListadoAsignaturasID:", minimoValorInput: 1, mensajeError: "\nEl valor debe ser mayor que 1", borrarInformacion: false);
+                            listadoAsignatura = Logica.ListadoAsignatura.ListarUna(listadoAsignaturasID: ID);
+                            if (listadoAsignatura == null)
+                            {
+                                MetodosComunes.MensajeColor(mensaje: "\nLa Asignatura del listado no existe.", color: ConsoleColor.Red);
+                            }
+                        } while (listadoAsignatura == null);
+
+                        if (MetodosInformar.InformarTodosAlumnos() == 0)
+                        {
+                            MetodosComunes.MensajeColor(mensaje: "\nLa lista de Alumnos esta vacia. Se debe tener al menos una asignatura en la tabla Alumnos.", color: ConsoleColor.Red);
+                            MetodosComunes.Continuar();
+                        }
+                        else
+                        {
+                            do
+                            {
+                                MetodosComunes.MensajeColor(mensaje: $"\nAnterior AlumnoID: {asignatura.AlumnoId}");
+                                ID = asignatura.AlumnoId = MetodosComunes.ValidacionNumericaInt(mensajeIngreso: "\nNuevo AlumnoID:", minimoValorInput: 1, mensajeError: "\nEl valor debe ser mayor que 1", borrarInformacion: false);
+                                alumno = Logica.Alumno.ListarUno(alumnoID: ID);
+                                if (alumno == null)
+                                {
+                                    MetodosComunes.MensajeColor(mensaje: "\nEl alumno no existe.", color: ConsoleColor.Red);
+                                }
+                            } while (alumno == null);
+
+                            if (MetodosInformar.InformarTodasCarreras() == 0)
+                            {
+                                MetodosComunes.MensajeColor(mensaje: "\nLa lista de Alumnos esta vacia. Se debe tener al menos una asignatura en la tabla Carreras.", color: ConsoleColor.Red);
+                                MetodosComunes.Continuar();
+                            }
+                            else
+                            {
+                                do
+                                {
+                                    MetodosComunes.MensajeColor(mensaje: $"\nAnterior CarreraID: {asignatura.CarreraId}");
+                                    ID = asignatura.CarreraId = MetodosComunes.ValidacionNumericaInt(mensajeIngreso: "\nNueva CarreraID:", minimoValorInput: 1, mensajeError: "\nEl valor debe ser mayor a 1.", borrarInformacion: false);
+                                    carrera = Logica.Carrera.ListarUna(carreraID: ID);
+                                    if (carrera == null)
+                                    {
+                                        MetodosComunes.MensajeColor(mensaje: "\nLa carrera no existe.", color: ConsoleColor.Red);
+                                    }
+
+                                } while (carrera == null);
+                                MetodosComunes.MensajeColor(mensaje: $"\nAnterior Comision: {asignatura.Comision}");
+                                asignatura.Comision = MetodosComunes.ValidacionNumericaInt(mensajeIngreso: "\nNueva Comision:", minimoValorInput: 1, mensajeError: "\nEl valor debe ser mayor a 1.");
+
+                                Console.WriteLine("\nIngrese el nuevo horario de entrada de la materia");
+                                MetodosComunes.MensajeColor(mensaje: $"\nAnterior Hora de entrada: {asignatura.HorarioEntrada.Hours}");
+                                horas = MetodosComunes.ValidacionNumericaInt(mensajeIngreso: "\nIndique la nueva hora entre 0 y 23:", mensajeError: "\nIngrese un valor entre 0 y 23", minimoValorInput: 0, maximoValorInput: 23);
+                                MetodosComunes.MensajeColor(mensaje: $"\nAnterior Minutos de entrada: {asignatura.HorarioEntrada.Minutes}");
+                                minutos = MetodosComunes.ValidacionNumericaInt(mensajeIngreso: "\nIndique los nuevos minutos entre 0 y 59:", mensajeError: "\nIngrese un valor entre 0 y 59", minimoValorInput: 0, maximoValorInput: 59);
+                                asignatura.HorarioEntrada = new TimeSpan(hours: horas, minutes: minutos, seconds: 0);
+
+                                Console.WriteLine("\nIngrese el nuevo horario de salida de la materia");
+                                MetodosComunes.MensajeColor(mensaje: $"\nAnterior Hora de salida: {asignatura.HorarioSalida.Hours}");
+                                horas = MetodosComunes.ValidacionNumericaInt(mensajeIngreso: "\nIngrese la hora entre 0 y 23:", mensajeError: "\nIngrese un valor entre 0 y 23", minimoValorInput: 0, maximoValorInput: 23);
+                                MetodosComunes.MensajeColor(mensaje: $"\nAnterior Minutos de salida: {asignatura.HorarioSalida.Minutes}");
+                                minutos = MetodosComunes.ValidacionNumericaInt(mensajeIngreso: "\nIngrese los minutos entre 0 y 59:", mensajeError: "\nIngrese un valor entre 0 y 59", minimoValorInput: 0, maximoValorInput: 59);
+
+                                asignatura.HorarioSalida = new TimeSpan(hours: horas, minutes: minutos, seconds: 0);
+                                MetodosComunes.MensajeColor(mensaje: $"\nAnteriores Dias: {asignatura.Dias}");
+                                asignatura.Dias = MetodosComunes.ValidacionTexto(mensajeIngreso: "\nNuevos Dias:", mensajeError: "\nEl valor debe estar comprendido en Lunes-Martes-Miercoles-Jueves-Viernes-Sabado-Domingo");
+
+                                devolucionEditar = Logica.Asignatura.Editar(asignatura);
+                                if (devolucionEditar.Contains("correctamente"))
+                                {
+                                    MetodosComunes.MensajeColor(mensaje: $"\nLa asignatura con ID: {asignatura.AsignaturaId} AlumnoID: {asignatura.AlumnoId} Comision: {asignatura.Comision} ha sido editaado correctamente.");
+                                }
+                                else
+                                {
+                                    MetodosComunes.MensajeColor(mensaje: $"\n{devolucionEditar}", color: ConsoleColor.Red);
+                                }
+                            }
+                        }
+
                     }
                 }
             }
