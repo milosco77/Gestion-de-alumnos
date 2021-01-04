92ceadff130642e00d5d4ef74ac84e785901d01c
Patricio Cordoba <miloscordoba@gmail.com>
2021-01-04 16:31:30 -0300

    se cambia nombre de la connection string

--- MVCCore/Startup.cs
+++ MVCCore/Startup.cs
@@ -24,7 +24,7 @@ namespace MVCCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<MVCCore.DatabaseFirstModel.AlumnosContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<MVCCore.DatabaseFirstModel.AlumnosContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DeveloperConnection")));
 
             services.AddControllersWithViews();
         }
